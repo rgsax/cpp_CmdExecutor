3d48feb377ba1c3856b08579543311e0c5fa27af
Riccardo Giordano <riccardog.sax@gmail.com>
2019-02-11 22:44:52 +0100

    updated output retrieving method

--- CmdExecutor.hpp
+++ CmdExecutor.hpp
@@ -17,54 +17,55 @@ using std::string;
 using std::system;
 using std::istreambuf_iterator;
 using std::runtime_error;
+using std::istream;
 
 class CmdExecutor {
 public:
 	static CmdExecutor* getInstance() {
 		if(instance == nullptr)
 			instance = new CmdExecutor();
-		
+
 		return instance;
 	}
-	
+
 	int execute(string command) {
 		command += " > " + output_path; //The output will be redirected to the temporary file
 		int status = system(command.c_str());
-		
+
 		collectOutput();
-		
+
 		return status;
 	}
-	
-	string getOutput() {
-		if(output == nullptr)
-			throw runtime_error("Missing execution before output request");
-			
-		return *output;
+
+	istream* getOutputStream() {
+        if(output_stream == nullptr)
+            throw runtime_error("Missing execution before output stream request");
+
+        return output_stream;
 	}
-	
+
 	~CmdExecutor() {
-		delete output;
+		delete output_stream;
 	}
-	
+
 private:
 	static CmdExecutor *instance;
 	static const string OUTPUT_FILENAME;
-	
-	string *output = nullptr;
+
+    istream *output_stream = nullptr;
+
 	string output_path;
-	
-	
+
+
 	void collectOutput() {
-		delete output; //Prevent memory leak
-		ifstream out_file(output_path); //Open the temporary file containing the output of the command
-		output = new string((istreambuf_iterator<char>(out_file)), istreambuf_iterator<char>()); // Put the output into a string
+		delete output_stream; //Prevent memory leak
+		output_stream = new ifstream(output_path); //Open the temporary file containing the output of the command using a stream
 	}
-	
-	CmdExecutor() { 
+
+	CmdExecutor() {
 		output_path = temp_directory_path().string() + "/" + OUTPUT_FILENAME; //The temporary file for the output
 	}
-	
+
 	CmdExecutor(const CmdExecutor &c) {	}
 
 };
